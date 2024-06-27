651669e10649854fbb1bfadca07ce0b31dc915a5
NamGK <namkamptner@gmail.com>
2024-06-27 18:19:02 +0200

    implement taking user input for DB Credentials, to avoid publishing creds.

--- src/util/DatabaseUtil.java
+++ src/util/DatabaseUtil.java
@@ -3,13 +3,19 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class DatabaseUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/tcg";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "postgres";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter PostgreSQL username: ");
+        String user = scanner.nextLine();
+
+        System.out.println("Enter PostgreSQL password: ");
+        String pass = scanner.nextLine();
+        return DriverManager.getConnection(URL, user, pass);
     }
 }
