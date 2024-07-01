b2169b7cfd2726cdb8e24948a0f724543c422ab6
Nam G. Kamptner <namkamptner@gmail.com>
2024-07-01 19:10:35 +0200

    Added following functions to deck. - addCard() for adding card to the deck. - removeCard() for removing card from deck. - getDeckCards() to return List<> of Card IDs. - getCardCounts() to return Map<Integer, Integer> with card id and occurrences of each card. - getSize() to return number of cards in deck. - getCount() to return number of occurrences of given Card ID. - toString() to override for string construction giving deck name, list of cards and map of card counts.

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -2,20 +2,60 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Deck {
     private static final int size = 40;
-    private List<Card> deck;
+    private String deckName;
+    private List<Integer> deckCards;
+    private Map<Integer, Integer> cardCounts;
 
-    public Deck() {
-        this.deck = new ArrayList<Card>();
+    public Deck(String deckName) {
+        this.deckName = deckName;
+        this.deckCards = new ArrayList<Integer>();
+        this.cardCounts = new HashMap<>();
     }
 
-    public void addCard(Card card) {
-        if (!(getCardCount(card) == 3)) {
-            deck.add(card);
+    public void addCard(int cardId) {
+        if (getCount(cardId) < 3) {
+            deckCards.add(cardId);
+            cardCounts.put(cardId, cardCounts.getOrDefault(cardId, 0) + 1);
         } else {
-            throw new IllegalStateException("Deck already contains max. copies (3) of " + card.getName());
+            throw new IllegalStateException("Cannot add more than 3 copies of the card with id " + cardId + ".");
         }
+    }
+
+    public void removeCard(int cardId) {
+        if (cardCounts.containsKey(cardId)) {
+            deckCards.remove(cardId);
+            if (cardCounts.get(cardId) > 1) {
+                cardCounts.put(cardId, cardCounts.get(cardId) - 1);
+            } else {
+                cardCounts.remove(cardId);
+            }
+        }
+    }
+
+    public List<Integer> getDeckCards() {
+        return deckCards;
+    }
+    public Map<Integer, Integer> getCardCounts() {
+        return cardCounts;
+    }
+    public int getSize() {
+        return deckCards.size();
+    }
+
+    public int getCount(int cardId) {
+        return cardCounts.getOrDefault(cardId, 0);
+    }
+
+    public String toString() {
+        return "Deck{" +
+                "Deck Name: " + deckName +
+                ", deck cards: " + deckCards +
+                ", card counts: " + cardCounts +
+                "}";
     }
 }
