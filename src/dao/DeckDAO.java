18aff4c7704a54ccbd2cf99e2f6ed8bf0fde20a8
NamGK <namkamptner@gmail.com>
2024-07-03 17:02:15 +0200

    DeckDAO implementation to organize deck database features.

---
+++ src/dao/DeckDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.Deck;
+import util.DatabaseUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeckDAO {
+
+    public void addDeck(Deck deck) {
+    }
+}
